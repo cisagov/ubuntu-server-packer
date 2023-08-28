e68f512abfeba8550152a6756191ff13a0442a3c
bra1ncramp <52966515+bra1ncramp@users.noreply.github.com>
2023-08-28 12:14:46 -0400

    Use long-form options in command
    Co-authored-by: Shane Frasier <jeremy.frasier@gwe.cisa.dhs.gov>

--- src/post_setup.sh
+++ src/post_setup.sh
@@ -7,6 +7,6 @@ rm --force /root/.ssh/authorized_keys
 sudo snap switch --channel=candidate amazon-ssm-agent
 sudo snap refresh amazon-ssm-agent
 [[ -d /etc/amazon/ssm ]] || sudo mkdir --parents /etc/amazon/ssm
-sudo cp -RT /snap/amazon-ssm-agent/current/ /etc/amazon/ssm/
+sudo cp --recursive --no-target-directory /snap/amazon-ssm-agent/current/ /etc/amazon/ssm/
 sudo cp /snap/amazon-ssm-agent/current/seelog.xml.template /etc/amazon/ssm/seelog.xml
 sudo snap restart amazon-ssm-agent
