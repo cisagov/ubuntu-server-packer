eb1c67f4db1a70b86b00da86f74cfbc6bd4d843b
bra1ncramp <mjgolla@outlook.com>
2023-08-31 13:36:52 -0400

    Add clarifying comments

--- src/post_setup.sh
+++ src/post_setup.sh
@@ -1,5 +1,10 @@
 #!/bin/bash
 
+# The amazon-ssm-agent package in the stable channel is typically out-dated.
+# In order to pull the latest stable version, Amazon recommends swithing to the candidate channel.
+# Also, the snap version of amazon-ssm-agent does not actually sync correctly with the service.
+# Amazon recommends copying the data from the snap dir to one under /etc in order to collect logs correctly
+# https://docs.aws.amazon.com/systems-manager/latest/userguide/agent-install-ubuntu-64-snap.html
 sudo snap switch --channel=candidate amazon-ssm-agent
 sudo snap refresh amazon-ssm-agent
 [[ -d /etc/amazon/ssm ]] || sudo mkdir --parents /etc/amazon/ssm
